81653e408c20bb7dc80080f264e5e11c862f7782
Guilstavo <gustavoartinis@gmail.com>
2015-09-24 20:26:28 -0300

    Storyboard pt. 5

--- estudoObjective-cAlura/ViewController.m
+++ estudoObjective-cAlura/ViewController.m
@@ -12,11 +12,11 @@
 @implementation ViewController
 
 -(IBAction)adiciona{
-    NSString *nome = [self.nome text];
-    NSString *endereco = [self.endereco text];
-    NSString *site = [self.site text];
-    NSString *telefone = [self.telefone text];
-    NSString *email = [self.email text];
+    NSString *nome = self.nome.text;
+    NSString *endereco = self.endereco.text;
+    NSString *site = self.site.text;
+    NSString *telefone = self.telefone.text;
+    NSString *email = self.email.text;
     NSLog(@"Dados do contato: %@ %@ %@ %@ %@", nome, endereco, site, telefone, email);
 }
 
